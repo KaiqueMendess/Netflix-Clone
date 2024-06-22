cb582d6c298a58e6c30e730cb11de367b3bbbe70
Kaique <84601420+KaiqueMendess@users.noreply.github.com>
2024-06-22 00:39:08 -0300

    Versão 1.0.1
    Melhorias visuais

--- home.php
+++ home.php
@@ -8,52 +8,48 @@ include('verifica_login.php');
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1,0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Netflix</title>
     <link rel="stylesheet" href="home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
+    <div class="container">
+        <!-- Início do Hero -->
+        <div class="header">
+            <div class="logo">
+                <img src="img/logo.png" alt="Netflix Logo">
+            </div>
 
-<div class="container">
-    <!-- Inicio Do Hero -->
-    <div class="header">
+            <!-- Menu -->
+            <div class="menu">
+                <!-- Menu Left -->
+                <div class="menu-left">
+                    <ul>
+                        <li>Início</li>
+                        <li>Séries</li>
+                        <li>Filmes</li>
+                        <li>Recentes</li>
+                        <li>Minha Lista</li>
+                    </ul>
+                </div> <!-- Fim do Menu Left -->
 
-        <div class="logo">
-            <img src="img/logo.png" alt="">
-        </div>
+                <!-- Menu Right -->
+                <div class="menu-right">
+                    <ul>
+                        <li><i class="fas fa-search"></i></li>
+                        <li>Infantil</li>
+                        <li><i class="fas fa-gift"></i></li>
+                        <li><i class="fas fa-bell"></i></li>
+                        <li class="user">
+                            <img src="img/user.png" alt="User Icon">
+                            <i class="fas fa-sort-down"></i>
+                        </li>
+                    </ul>
+                </div> <!-- Fim do Menu Right -->
 
-        <!-- menu -->
-        <div class="menu">
-
-        <!-- menu left -->
-        <div class="menu-left">
-            <ul>
-                <li>Inicio</li>
-                <li>Séries</li>
-                <li>Filmes</li>
-                <li>Recentes</li>
-                <li>Minha Lista</li>
-            </ul>
-        </div> <!-- fim do menu left -->
-
-        <div class="menu-right">
-            <ul>
-                <li> <i class="fas fa-search" ></i> </li>
-                <li>Infantil</li>
-                <li> <i class="fas fa-gift" ></i> </li>
-                <li> <i class="fas fa-bell" ></i> </li>
-                <li class="user">
-                    <img src="img/user.png" alt="">
-                    <i class="fas fa-sort-down"></i>
-                </li>
-            </ul>
-        </div> <!-- Fecha menu -->
-
-        </div>
-
-    </div>
-</div>
-
+            </div> <!-- Fim do Menu -->
+        </div> <!-- Fim do Header -->
+    </div> <!-- Fim do Container -->
 </body>
 </html>
--- netflix.php
+++ netflix.php
@@ -1,17 +1,13 @@
 <?php
-/* esse bloco de código em php verifica se existe a sessão, pois o usuário pode
- simplesmente não fazer o login e digitar na barra de endereço do seu navegador
-o caminho para a página principal do site (sistema), burlando assim a obrigação de
-fazer um login, com isso se ele não estiver feito o login não será criado a session,
-então ao verificar que a session não existe a página redireciona o mesmo
- para a index.php.*/
 session_start();
-if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true))
-{
-  header('location:php/index.php');
-  }
 
-$logado = $_SESSION['login'];
+// Verifica se a sessão 'login' e 'senha' estão definidas
+if (!isset($_SESSION['login']) || !isset($_SESSION['senha'])) {
+    header('Location: php/index.php'); // Redireciona para a página de login
+    exit(); // Encerra o script para evitar que o restante da página seja carregado
+}
+
+$logado = $_SESSION['login']; // Obtém o nome do usuário logado
 ?>
 
 <!DOCTYPE html>
@@ -20,40 +16,29 @@ $logado = $_SESSION['login'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/main.css">
-
-    <!--responsividade-->
     <link rel="stylesheet" href="style/responsive.css">
-
-    <!--owl css-->
     <link rel="stylesheet" href="style/owl/owl.carousel.min.css">
     <link rel="stylesheet" href="style/owl/owl.theme.default.min.css">
     <link rel="shortcut icon" href="img/netflix.png" type="image/x-icon" />
-
     <title>NETFLIX</title>
 </head>
 <body>
-         <header>
+    <header>
         <div class="container">
-            <img class="logo" src="img/logo.png">
+            <img class="logo" src="img/logo.png" alt="Netflix Logo">
             <nav>
-                <a href="#">Inicio</a>
+                <a href="#">Início</a>
                 <a href="#">Séries</a>
                 <a href="#">Filmes</a>
                 <a href="#">Documentários</a>
                 <a href="logout.php">Sair</a>
-                <?php
-                echo" Olá $logado";
-                ?>
-
-                
-                
+                <span>Olá <?php echo $logado; ?></span>
             </nav>
         </div>
     </header>
   
     <main>
         <div class="filme-principal">
-       
             <div class="container">
                 <h3 class="titulo">GODZILLA VS KONG</h3>
                 <p class="descricao"></p>
@@ -74,84 +59,28 @@ $logado = $_SESSION['login'];
     <div class="carrosel-filmes">
         <h2>Lançamentos</h2>
         <div class="owl-carousel owl-theme">
+            <?php for ($i = 1; $i <= 10; $i++) : ?>
                 <div class="item">
-                    <img class="box-filme" src="img/lancamento1.jpg" alt="" srcset="">
+                    <img class="box-filme" src="img/lancamento<?php echo $i; ?>.jpg" alt="Lançamento <?php echo $i; ?>">
                 </div>
-                <div class="item">
-                    <img class="box-filme" src="img/lancamento2.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/lancamento3.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/lancamento4.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/lancamento5.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/lancamento6.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/lancamento7.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/lancamento8.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/lancamento9.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/lancamento10.jpg" alt="" srcset="">
-                </div>
+            <?php endfor; ?>
         </div>
     </div>
-    
-    
+
     <div class="carrosel-filmes2">
         <h2>Filmes</h2>
         <div class="owl-carousel owl-theme">
+            <?php for ($i = 1; $i <= 10; $i++) : ?>
                 <div class="item">
-                    <img class="box-filme" src="img/filme1.jpg" alt="" srcset="">
+                    <img class="box-filme" src="img/filme<?php echo $i; ?>.jpg" alt="Filme <?php echo $i; ?>">
                 </div>
-                <div class="item">
-                    <img class="box-filme" src="img/filme2.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/filme3.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/filme4.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/filme5.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/filme6.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/filme7.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/filme8.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/filme9.jpg" alt="" srcset="">
-                </div>
-                <div class="item">
-                    <img class="box-filme" src="img/filme10.jpg" alt="" srcset="">
-                </div>
+            <?php endfor; ?>
         </div>
     </div>
-
-
 
     <script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script>
     <script src="js/owl/jquery.min.js"></script>
     <script src="js/owl/owl.carousel.min.js"></script>
     <script src="js/owl/setup.js"></script>
- 
-
- 
 </body>
 </html>
